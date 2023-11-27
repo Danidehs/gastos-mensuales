cfe30d080f1c7f49c8c58250836e177bec604fa3
Daniel Hernandez Stalhuth <hsdaniele95@gmail.com>
2023-11-27 02:58:43 +0100

    Agregar toggle button para determinar si es un ingreso o un gasto

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,9 +1,11 @@
-import React, { useState, useContext } from "react";
-import { GlobalContext } from "../context/GlobalState";
+import React, { useState, useContext } from 'react';
+import Switch from 'react-switch';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
+  const [isIncome, setIsIncome] = useState(false); // Default to expense
 
   const { addTransaction } = useContext(GlobalContext);
 
@@ -12,7 +14,7 @@ export const AddTransaction = () => {
     const newTransaction = {
       id: Math.floor(Math.random() * 10000000),
       text,
-      amount: -amount,
+      amount: isIncome ? amount : -amount,
     };
 
     addTransaction(newTransaction);
@@ -22,28 +24,49 @@ export const AddTransaction = () => {
     <>
       <h3>Agregar transaccion</h3>
       <form onSubmit={onSubmit}>
-        <div className="form-control">
-          <label htmlFor="text">Concepto</label>
+        <div className='form-control'>
+          <label htmlFor='text'>Concepto</label>
           <input
-            type="text"
+            type='text'
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
+            placeholder='Ingresa un concepto...'
           />
         </div>
-        <div className="form-control">
-          <label htmlFor="amount">
+        <div className='form-control'>
+          <label htmlFor='amount'>
             Monto <br />
-            (Utilizar simbolo negativo "-" para ingresos)
           </label>
           <input
-            type="number"
+            type='number'
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
-            placeholder="Enter amount..."
+            placeholder='Enter amount...'
           />
         </div>
-        <button className="btn">Agregar</button>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '50px',
+          }}
+          className='form-control'
+        >
+          <label>
+            <Switch
+              onChange={() => setIsIncome(!isIncome)}
+              checked={isIncome}
+              onColor='#4CAF50' // Green for income
+              offColor='#FF6347' // Soft red for expenses
+              uncheckedIcon={<div>Gasto</div>}
+              checkedIcon={<div>Ingreso</div>}
+              width={100}
+            />
+          </label>
+          <span>{isIncome ? 'Ingreso' : 'Gasto'}</span>
+        </div>
+        <button className='btn'>Agregar</button>
       </form>
     </>
   );
