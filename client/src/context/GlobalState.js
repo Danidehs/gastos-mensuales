3b9680541d84b36fe6237c4902ed7857bc153396
Daniel Hernandez Stalhuth <hsdaniele95@gmail.com>
2024-02-21 03:35:07 +0100

    vercel serverless backend testing

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -21,7 +21,7 @@ export const GlobalProvider = ({ children }) => {
   // actions
   async function getTransactions() {
     try {
-      const res = await axios.get('/api/v1/transactions');
+      const res = await axios.get('/api/getTransactions');
 
       dispatch({
         type: 'GET_TRANSACTION',
@@ -37,7 +37,7 @@ export const GlobalProvider = ({ children }) => {
 
   async function deleteTransaction(id) {
     try {
-      await axios.delete(`/api/v1/transactions/${id}`);
+      await axios.delete(`/api/deleteTransaction?id=${id}`);
 
       dispatch({
         type: 'DELETE_TRANSACTION',
@@ -53,12 +53,12 @@ export const GlobalProvider = ({ children }) => {
 
   async function addTransaction(transaction) {
     const config = {
-      header: {
+      headers: {
         'Content-Type': 'application/json',
       },
     };
     try {
-      const res = await axios.post(`/api/v1/transactions`, transaction, config);
+      const res = await axios.post(`/api/addTransaction`, transaction, config);
 
       dispatch({
         type: 'ADD_TRANSACTION',
--- config/db.js
+++ config/db.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+let cached = global.mongoose;
 
-    console.log(
-      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
-    );
-  } catch (err) {
-    console.log(`Error: ${err.message}`.red);
-    process.exit(1);
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  console.log('hi');
+  if (cached.conn) {
+    // Use existing database connection
+    return cached.conn;
   }
-};
+
+  if (!cached.promise) {
+    // If no connection is in progress, start a new connection
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+
+  // Use new database connection
+  cached.conn = await cached.promise;
+  console.log(`MongoDB Connected: ${cached.conn.connection.host}`);
+  return cached.conn;
+}
 
 module.exports = connectDB;
