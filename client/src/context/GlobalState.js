292d0244f5406d5b6e6c4f109060ed3ce5fb3d4e
Daniel Hernandez Stalhuth <hsdaniele95@gmail.com>
2024-02-21 19:24:57 +0100

    vercel configs

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -10,11 +10,9 @@ const initialState = {
 };
 
 // create context
-
 export const GlobalContext = createContext(initialState);
 
 // provider component
-
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
