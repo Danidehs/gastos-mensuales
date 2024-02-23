29b747c94a33ec15a69d1869fdf3eb5ef1abff55
Daniel Hernandez Stalhuth <hsdaniele95@gmail.com>
2024-02-23 20:25:27 +0100

    test

--- api/getTransactions.js
+++ api/getTransactions.js
@@ -1,34 +1,41 @@
-// const mongoose = require('mongoose');
 // const cors = require('cors')({ origin: true });
+
+// const mongoose = require('mongoose');
 // const Transaction = require('../models/Transaction');
 // const connectDB = require('../config/db');
 
-// export function GET(req, res) {
-//   return new res.status(200).json({
-//     message: 'Function executed successfully',
-//   });
-// }
+// module.exports = async (req, res) => {
+//   console.log('get working');
 
-const mongoose = require('mongoose');
-const Transaction = require('../models/Transaction');
-const connectDB = require('../config/db');
+//   await connectDB();
 
-module.exports = async (req, res) => {
-  console.log('get working');
+//   try {
+//     const transactions = await Transaction.find();
+//     return res.status(200).json({
+//       success: true,
+//       count: transactions.length,
+//       data: transactions,
+//     });
+//   } catch (err) {
+//     return res.status(500).json({
+//       success: false,
+//       error: 'Server Error',
+//     });
+//   }
+// };
+
+export default async function handler(req, res) {
+  console.log('add working');
+
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust as needed
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   await connectDB();
 
-  try {
-    const transactions = await Transaction.find();
-    return res.status(200).json({
-      success: true,
-      count: transactions.length,
-      data: transactions,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      error: 'Server Error',
-    });
+  res.json({ message: 'It works (add)' });
+  // Handle pre-flight requests for CORS
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
   }
-};
+}
